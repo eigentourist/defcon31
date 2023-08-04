2766f11d4075fb949f0960f85943a2fb17f5ada6
eigentourist <eigentourist@gmail.com>
2023-08-04 05:43:42 +0100

    More functionality for two.s

--- two.s
+++ two.s
@@ -5,13 +5,28 @@
 
 .extern printf
 
+.data
+    // Try changing this number to see how the two's complement formula
+    // works. The program will show you the unsigned (two's complement)
+    // number that the negative number translates to. Or, in more mathematical
+    // terms, the two's complement value of any number a is 2^n - a, where n
+    // is the number of bits in the integer. Here, n is 64, because we are
+    // working with the full width of the 64-bit registers of the processor.
+    integer: .quad -16
+    // An easier way to think about it is that 
+
 .text
 
 .global _start
 _start:
+    // Print intro message
+    ldr x0, =msg
+    bl printf
+
     // Load the value of the CPU register you want to print into x5
     // For example, if you want to print the value of register x1, load it into x5
-    mov x5, #65535
+    ldr x0, =integer
+    ldr x5, [x0]
 
     // Call the function to print the binary representation
     // Be careful about the order in which you call these functions
@@ -35,7 +50,8 @@ print_newline:
 
 print_signed:
     ldr x0, =signed_int_str
-    mov x1, #65535
+    ldr x2, =integer
+    ldr x1, [x2]
     str lr, [sp, #-16]! // push link register so we can return
     bl printf           // call printf function
     ldr lr, [sp], #16   // pop link register value so we can return
@@ -43,7 +59,8 @@ print_signed:
 
 print_unsigned:
     ldr x0, =unsigned_int_str
-    mov x1, #65535
+    ldr x2, =integer
+    ldr x1, [x2]
     str lr, [sp, #-16]! // push link register
     bl printf
     ldr lr, [sp], #16   // pop link register
@@ -73,6 +90,7 @@ print_loop:
     ret                // All done printing, so return
 
 .data
+msg:  .asciz "The negative representation of a number A in two's complement is\nan unsigned number B which, when A is added to it, equals zero.\n\n"
 eol:  .ascii "\n"
 signed_int_str:   .asciz "Signed value: %d\n"
 unsigned_int_str: .asciz "Unsigned value: %u\n"
